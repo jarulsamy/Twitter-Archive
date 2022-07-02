285b1b1365d991b2c6dbfe49d1ff51ac94f57026
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2022-07-02 15:35:17 -0600

    Add interactive authentication

--- src/main.py
+++ src/main.py
@@ -1,12 +1,78 @@
-import tweepy
 from dotenv import load_dotenv
-import os
-from pprint import pprint
-import json
 from json import JSONEncoder
 from pathlib import Path
-import time
 import datetime
+import json
+import os
+import time
+import tweepy
+
+import http.server
+import socketserver
+
+# DEBUG
+from pprint import pprint
+
+
+class OneShotTCPServer(socketserver.TCPServer):
+    def server_activate(self) -> None:
+        raise NotImplementedError(
+            "Use socketserver.TCPServer to handle several requests instead."
+        )
+
+    def process_request(self, request, client_address):
+        """Call finish_request."""
+        handler_inst = self.finish_request(request, client_address)
+        self.shutdown_request(request)
+        return handler_inst
+
+    def finish_request(self, request, client_address):
+        return self.RequestHandlerClass(request, client_address, self)
+
+    def _handle_request_noblock(self):
+        """Handle one request, without blocking."""
+        try:
+            request, client_address = self.get_request()
+        except OSError:
+            return
+
+        if self.verify_request(request, client_address):
+            try:
+                # Almost the same as the standard library implementation,
+                # but returns an instance of the request handler.
+                return self.process_request(request, client_address)
+            except Exception:
+                self.handle_error(request, client_address)
+                self.shutdown_request(request)
+        else:
+            self.shutdown_request(request)
+
+
+class HTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    resp = r"""<!doctype html>
+<html lang="en">
+<h1>
+Success, you may close this tab!
+</h1>
+</html>
+    """.encode()
+
+    def __init__(self, *args, **kwargs):
+        self._token = None
+        super().__init__(*args, **kwargs)
+
+    def do_GET(self):
+        self._token = f"https://{self.address_string()}{self.path}"
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        self.wfile.write(self.resp)
+
+    @property
+    def token(self):
+        return self._token
 
 
 class TweetEncoder(JSONEncoder):
@@ -30,52 +96,61 @@ ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET", "")
 CLIENT_ID = os.environ.get("CLIENT_ID", "")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET", "")
 
-REDIRECT_URI = "https://localhost:8080"
+REDIRECT_PORT = 8080
+REDIRECT_URI = f"http://localhost:{REDIRECT_PORT}"
 
 
-# print(API_KEY)
-# print(API_KEY_SECRET)
-# print(BEARER_TOKEN)
-# print(ACCESS_TOKEN)
-# print(ACCESS_TOKEN_SECRET)
-# print(CLIENT_ID)
-# print(CLIENT_SECRET)
-
-
-def auth():
+def _oauth(save_path, headless=False):
     oauth2_user_handler = tweepy.OAuth2UserHandler(
         client_id=CLIENT_ID,
         redirect_uri=REDIRECT_URI,
         scope=["tweet.read", "users.read", "bookmark.read"],
         client_secret=CLIENT_SECRET,
     )
-
     print(oauth2_user_handler.get_authorization_url())
-    reponse_url = input("Enter the response url here: ")
-    access_token = oauth2_user_handler.fetch_token(reponse_url)
 
-    with open(CACHE_PATH, "w") as json_file:
-        json.dump(access_token, json_file)
+    if not headless:
+        # Start the HTTP server to receive the token.
+        handler = HTTPRequestHandler
+        with OneShotTCPServer(("", REDIRECT_PORT), handler) as httpd:
+            handler_inst = httpd.handle_request()
+            response_url = handler_inst.token
+    else:
+        response_url = input("Enter the response url here: ")
+
+    access_token = oauth2_user_handler.fetch_token(response_url)
+    with open(save_path, "w") as fp:
+        json.dump(access_token, fp)
 
     return access_token
 
 
-if not CACHE_PATH.is_file():
-    access_token = auth()
-else:
-    with open(CACHE_PATH, "r") as json_file:
-        access_token = json.load(json_file)
+def auth(
+    cache_path="access_token.json", use_cache=True, headless=False
+) -> tweepy.Client:
+    """Login to the Twitter API"""
+    cache_path = None if cache_path is None else Path(cache_path)
+    if not use_cache or not cache_path.is_file():
+        access_token = _oauth(cache_path, headless)
+    else:
+        # Try to use the cache if possible.
+        with open(cache_path, "r") as json_file:
+            access_token = json.load(json_file)
 
-    if float(access_token["expires_at"]) < time.time():
-        print("Cache expired, reauth")
-        access_token = auth()
+        # Check if expired, if so re-auth.
+        if float(access_token["expires_at"]) < time.time():
+            access_token = _oauth(cache_path, headless)
+
+    client = tweepy.Client(
+        access_token["access_token"],
+        wait_on_rate_limit=True,
+    )
+
+    return client
 
 
-print(access_token)
+client = auth()
 
-client = tweepy.Client(
-    access_token["access_token"],
-)
 resps = []
 media = {}
 tweets = []
@@ -84,9 +159,11 @@ page_token = None
 while True:
     resp = client.get_bookmarks(
         expansions=[
-            "author_id",
-            "referenced_tweets.id",
+            "attachments.poll_ids",
             "attachments.media_keys",
+            "author_id",
+            "geo.place_id",
+            "referenced_tweets.id",
         ],
         max_results=100,
         pagination_token=page_token,
@@ -100,10 +177,32 @@ while True:
             "alt_text",
             "variants",
         ],
-        tweet_fields=[
-            "author_id",
-            "created_at",
+        place_fields=[
+            "full_name",
+            "id",
+            "geo",
         ],
+        poll_fields=[
+            "id",
+            "options",
+            "duration_minutes",
+            "end_datetime",
+            "voting_status",
+        ],
+        tweet_fields=[
+            "id",
+            "text",
+            "attachments",
+            "author_id",
+            "conversation_id",
+            "created_at",
+            "geo",
+            "lang",
+            "possibly_sensitive",
+            "referenced_tweets",
+            "source",
+        ],
+        user_fields=["id"],
     )
     try:
         page_token = resp.meta["next_token"]
@@ -119,24 +218,16 @@ while True:
         pass
 
     for i in resp.data:
-        if (
-            i.get("attachments") is None
-            or i.get("attachments").get("media_keys") is None
-        ):
+        if i.get("attachments", {}).get("media_keys") is None:
             tweets.append(dict(i))
             continue
 
-
         tweet = dict(i)
-
         tweet["media"] = []
         for media_key in i["attachments"]["media_keys"]:
             tweet["media"].append(media[media_key].data)
 
         tweets.append(tweet)
 
-    # pprint(media)
-
-pprint(tweets)
 with open("bookmarks.json", "w") as fp:
     json.dump(tweets, fp, indent=2, cls=TweetEncoder)

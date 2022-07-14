a0c3f93171870007eaac72458ad70d0943e199ab
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2022-07-14 17:30:44 -0600

    Use new build tools
    Use pyproject.toml for the bulk of the build responsibilities.

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 from setuptools import setup
-from TwitterArchive import __version__
 
-
-install_requires = [
-    "python-dotenv >= 0.20.0",
-    "requests >= 2.28.1",
-    "tqdm >= 4.64.0",
-    "tweepy >= 4.10.0",
-]
-
-
-setup(
-    name="twitter-archive",
-    version=__version__,
-    description="A Suite of Twitter Archival Tools",
-    url="https://github.com/jarulsamy/twitter-bookmark-downloader",
-    packages=["TwitterArchive"],
-    install_requires=install_requires,
-    python_requires=">=3.9",
-    entry_points={"console_scripts": ["twitter-archive=TwitterArchive.cli:main"]},
-)
+setup()

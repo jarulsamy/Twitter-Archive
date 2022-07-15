d4cdf73073c4b837a387e77e36d9251fba276e58
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2022-07-15 03:40:57 -0600

    Add some more CLI opts
    + --no-clobber
    + --quiet
    + --num-download-threads

--- tests/unit/test_cil.py
+++ tests/unit/test_cil.py
@@ -4,8 +4,8 @@ import io
 import unittest
 from pathlib import Path
 
-from TwitterArchive.cli import build_parser
 from TwitterArchive import __version__
+from TwitterArchive.cli import build_parser
 
 
 class CLITestCase(unittest.TestCase):
@@ -70,6 +70,9 @@ class CLITestCase(unittest.TestCase):
                 "manifest_input": None,
                 "manifest_output": Path("bookmark-manifest.json"),
                 "media_output": Path("media"),
+                "no_clobber": False,
+                "quiet": False,
+                "num_download_threads": 8,
             },
         )
 
@@ -86,6 +89,9 @@ class CLITestCase(unittest.TestCase):
                 "manifest_input": None,
                 "manifest_output": Path("bookmark-manifest.json"),
                 "media_output": Path("foobar"),
+                "no_clobber": False,
+                "quiet": False,
+                "num_download_threads": 8,
             },
         )
 
@@ -102,6 +108,9 @@ class CLITestCase(unittest.TestCase):
                 "manifest_input": None,
                 "manifest_output": Path("bookmark-manifest.json"),
                 "media_output": Path("foobar"),
+                "no_clobber": False,
+                "quiet": False,
+                "num_download_threads": 8,
             },
         )
 
@@ -118,6 +127,9 @@ class CLITestCase(unittest.TestCase):
                 "manifest_input": None,
                 "manifest_output": Path("foobar"),
                 "media_output": Path("media"),
+                "no_clobber": False,
+                "quiet": False,
+                "num_download_threads": 8,
             },
         )
 
@@ -134,6 +146,9 @@ class CLITestCase(unittest.TestCase):
                 "manifest_input": None,
                 "manifest_output": Path("foobar"),
                 "media_output": Path("media"),
+                "no_clobber": False,
+                "quiet": False,
+                "num_download_threads": 8,
             },
         )
 
@@ -150,6 +165,9 @@ class CLITestCase(unittest.TestCase):
                 "manifest_input": Path("foobar"),
                 "manifest_output": Path("bookmark-manifest.json"),
                 "media_output": Path("media"),
+                "no_clobber": False,
+                "quiet": False,
+                "num_download_threads": 8,
             },
         )
 
@@ -166,6 +184,9 @@ class CLITestCase(unittest.TestCase):
                 "manifest_input": Path("foobar"),
                 "manifest_output": Path("bookmark-manifest.json"),
                 "media_output": Path("media"),
+                "no_clobber": False,
+                "quiet": False,
+                "num_download_threads": 8,
             },
         )
 
@@ -182,6 +203,9 @@ class CLITestCase(unittest.TestCase):
                 "manifest_input": None,
                 "manifest_output": Path("bookmark-manifest.json"),
                 "media_output": Path("media"),
+                "no_clobber": False,
+                "quiet": False,
+                "num_download_threads": 8,
             },
         )
 
@@ -205,6 +229,9 @@ class CLITestCase(unittest.TestCase):
                 "manifest_input": None,
                 "manifest_output": Path("manifest-output"),
                 "media_output": Path("media-output"),
+                "no_clobber": False,
+                "quiet": False,
+                "num_download_threads": 8,
             },
         )
 
@@ -226,5 +253,71 @@ class CLITestCase(unittest.TestCase):
                 "manifest_input": None,
                 "manifest_output": Path("manifest-output"),
                 "media_output": Path("media-output"),
+                "no_clobber": False,
+                "quiet": False,
+                "num_download_threads": 8,
+            },
+        )
+
+    def test_no_clobber(self):
+        parser = build_parser(False)
+
+        argv = ["--no-clobber"]
+        args = parser.parse_args(argv)
+        args = vars(args)
+        self.assertDictEqual(
+            args,
+            {
+                "headless": False,
+                "manifest_input": None,
+                "manifest_output": Path("bookmark-manifest.json"),
+                "media_output": Path("media"),
+                "no_clobber": True,
+                "quiet": False,
+                "num_download_threads": 8,
+            },
+        )
+
+    def test_num_download_threads(self):
+        parser = build_parser(False)
+
+        argv = ["--num-download-threads", "4"]
+        args = parser.parse_args(argv)
+        args = vars(args)
+        self.assertDictEqual(
+            args,
+            {
+                "headless": False,
+                "manifest_input": None,
+                "manifest_output": Path("bookmark-manifest.json"),
+                "media_output": Path("media"),
+                "no_clobber": False,
+                "quiet": False,
+                "num_download_threads": 4,
+            },
+        )
+
+    def test_invalid_num_download_threads_raises(self):
+        parser = build_parser(False)
+
+        argv = ["--num-download-threads", "-1"]
+        self.assertRaises(argparse.ArgumentError, parser.parse_args, argv)
+
+    def test_quiet(self):
+        parser = build_parser(False)
+
+        argv = ["--quiet"]
+        args = parser.parse_args(argv)
+        args = vars(args)
+        self.assertDictEqual(
+            args,
+            {
+                "headless": False,
+                "manifest_input": None,
+                "manifest_output": Path("bookmark-manifest.json"),
+                "media_output": Path("media"),
+                "no_clobber": False,
+                "quiet": True,
+                "num_download_threads": 8,
             },
         )

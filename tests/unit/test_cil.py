69e240f80d573f88d479ba754ef3b4933f6c568f
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2022-07-20 03:25:35 -0600

    Reduce code duplication

--- tests/unit/test_cil.py
+++ tests/unit/test_cil.py
@@ -8,19 +8,20 @@ from TwitterArchive import __version__
 from TwitterArchive.cli import build_parser
 
 
+def _default_expected_args():
+    return {
+        "headless": False,
+        "manifest_input": None,
+        "manifest_output": Path("bookmark-manifest.json"),
+        "media_output": Path("media"),
+        "no_clobber": False,
+        "quiet": False,
+        "num_download_threads": 8,
+        "verbose": 0,
+    }
+
+
 class CLITestCase(unittest.TestCase):
-    def test_help_short(self):
-        # Setup
-        stdout = io.StringIO()
-
-        parser = build_parser(False)
-        argv = ["-h"]
-        with contextlib.redirect_stdout(stdout):
-            with self.assertRaises(SystemExit):
-                parser.parse_args(argv)
-
-        self.assertIn("help", stdout.getvalue())
-
     def test_help_long(self):
         # Setup
         stdout = io.StringIO()
@@ -51,19 +52,7 @@ class CLITestCase(unittest.TestCase):
         argv = []
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": None,
-                "manifest_output": Path("bookmark-manifest.json"),
-                "media_output": Path("media"),
-                "no_clobber": False,
-                "quiet": False,
-                "num_download_threads": 8,
-                "verbose": 0,
-            },
-        )
+        self.assertDictEqual(args, _default_expected_args())
 
     def test_media_arg_short(self):
         parser = build_parser(False)
@@ -71,19 +60,11 @@ class CLITestCase(unittest.TestCase):
         argv = ["-o", "foobar"]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": None,
-                "manifest_output": Path("bookmark-manifest.json"),
-                "media_output": Path("foobar"),
-                "no_clobber": False,
-                "quiet": False,
-                "num_download_threads": 8,
-                "verbose": 0,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["media_output"] = Path("foobar")
+
+        self.assertDictEqual(args, expected)
 
     def test_media_arg_long(self):
         parser = build_parser(False)
@@ -91,19 +72,11 @@ class CLITestCase(unittest.TestCase):
         argv = ["--media-output", "foobar"]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": None,
-                "manifest_output": Path("bookmark-manifest.json"),
-                "media_output": Path("foobar"),
-                "no_clobber": False,
-                "quiet": False,
-                "num_download_threads": 8,
-                "verbose": 0,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["media_output"] = Path("foobar")
+
+        self.assertDictEqual(args, expected)
 
     def test_manifest_arg_short(self):
         parser = build_parser(False)
@@ -111,19 +84,11 @@ class CLITestCase(unittest.TestCase):
         argv = ["-m", "foobar"]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": None,
-                "manifest_output": Path("foobar"),
-                "media_output": Path("media"),
-                "no_clobber": False,
-                "quiet": False,
-                "num_download_threads": 8,
-                "verbose": 0,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["manifest_output"] = Path("foobar")
+
+        self.assertDictEqual(args, expected)
 
     def test_manifest_arg_long(self):
         parser = build_parser(False)
@@ -131,19 +96,11 @@ class CLITestCase(unittest.TestCase):
         argv = ["--manifest-output", "foobar"]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": None,
-                "manifest_output": Path("foobar"),
-                "media_output": Path("media"),
-                "no_clobber": False,
-                "quiet": False,
-                "num_download_threads": 8,
-                "verbose": 0,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["manifest_output"] = Path("foobar")
+
+        self.assertDictEqual(args, expected)
 
     def test_manifest_input_short(self):
         parser = build_parser(False)
@@ -151,19 +108,11 @@ class CLITestCase(unittest.TestCase):
         argv = ["-i", "foobar"]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": Path("foobar"),
-                "manifest_output": Path("bookmark-manifest.json"),
-                "media_output": Path("media"),
-                "no_clobber": False,
-                "quiet": False,
-                "num_download_threads": 8,
-                "verbose": 0,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["manifest_input"] = Path("foobar")
+
+        self.assertDictEqual(args, expected)
 
     def test_manifest_input_long(self):
         parser = build_parser(False)
@@ -171,19 +120,11 @@ class CLITestCase(unittest.TestCase):
         argv = ["--manifest-input", "foobar"]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": Path("foobar"),
-                "manifest_output": Path("bookmark-manifest.json"),
-                "media_output": Path("media"),
-                "no_clobber": False,
-                "quiet": False,
-                "num_download_threads": 8,
-                "verbose": 0,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["manifest_input"] = Path("foobar")
+
+        self.assertDictEqual(args, expected)
 
     def test_headless(self):
         parser = build_parser(False)
@@ -191,19 +132,11 @@ class CLITestCase(unittest.TestCase):
         argv = ["--headless"]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": True,
-                "manifest_input": None,
-                "manifest_output": Path("bookmark-manifest.json"),
-                "media_output": Path("media"),
-                "no_clobber": False,
-                "quiet": False,
-                "num_download_threads": 8,
-                "verbose": 0,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["headless"] = True
+
+        self.assertDictEqual(args, expected)
 
     def test_mutual_exclusion(self):
         parser = build_parser(False)
@@ -218,19 +151,12 @@ class CLITestCase(unittest.TestCase):
         argv = ["-o", "media-output", "-m", "manifest-output"]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": None,
-                "manifest_output": Path("manifest-output"),
-                "media_output": Path("media-output"),
-                "no_clobber": False,
-                "quiet": False,
-                "num_download_threads": 8,
-                "verbose": 0,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["media_output"] = Path("media-output")
+        expected["manifest_output"] = Path("manifest-output")
+
+        self.assertDictEqual(args, expected)
 
     def test_many_long(self):
         parser = build_parser(False)
@@ -243,19 +169,12 @@ class CLITestCase(unittest.TestCase):
         ]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": None,
-                "manifest_output": Path("manifest-output"),
-                "media_output": Path("media-output"),
-                "no_clobber": False,
-                "quiet": False,
-                "num_download_threads": 8,
-                "verbose": 0,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["media_output"] = Path("media-output")
+        expected["manifest_output"] = Path("manifest-output")
+
+        self.assertDictEqual(args, expected)
 
     def test_no_clobber(self):
         parser = build_parser(False)
@@ -263,19 +182,11 @@ class CLITestCase(unittest.TestCase):
         argv = ["--no-clobber"]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": None,
-                "manifest_output": Path("bookmark-manifest.json"),
-                "media_output": Path("media"),
-                "no_clobber": True,
-                "quiet": False,
-                "num_download_threads": 8,
-                "verbose": 0,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["no_clobber"] = True
+
+        self.assertDictEqual(args, expected)
 
     def test_num_download_threads(self):
         parser = build_parser(False)
@@ -283,19 +194,11 @@ class CLITestCase(unittest.TestCase):
         argv = ["--num-download-threads", "4"]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": None,
-                "manifest_output": Path("bookmark-manifest.json"),
-                "media_output": Path("media"),
-                "no_clobber": False,
-                "quiet": False,
-                "num_download_threads": 4,
-                "verbose": 0,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["num_download_threads"] = 4
+
+        self.assertDictEqual(args, expected)
 
     def test_invalid_num_download_threads_raises(self):
         parser = build_parser(False)
@@ -309,19 +212,11 @@ class CLITestCase(unittest.TestCase):
         argv = ["--quiet"]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": None,
-                "manifest_output": Path("bookmark-manifest.json"),
-                "media_output": Path("media"),
-                "no_clobber": False,
-                "quiet": True,
-                "num_download_threads": 8,
-                "verbose": 0,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["quiet"] = True
+
+        self.assertDictEqual(args, expected)
 
     def test_verbose(self):
         parser = build_parser(False)
@@ -329,19 +224,11 @@ class CLITestCase(unittest.TestCase):
         argv = ["-v"]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": None,
-                "manifest_output": Path("bookmark-manifest.json"),
-                "media_output": Path("media"),
-                "no_clobber": False,
-                "quiet": False,
-                "num_download_threads": 8,
-                "verbose": 1,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["verbose"] = 1
+
+        self.assertDictEqual(args, expected)
 
     def test_verbose_multiple_short(self):
         parser = build_parser(False)
@@ -349,19 +236,11 @@ class CLITestCase(unittest.TestCase):
         argv = ["-vvv"]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": None,
-                "manifest_output": Path("bookmark-manifest.json"),
-                "media_output": Path("media"),
-                "no_clobber": False,
-                "quiet": False,
-                "num_download_threads": 8,
-                "verbose": 3,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["verbose"] = 3
+
+        self.assertDictEqual(args, expected)
 
     def test_verbose_multiple_long(self):
         parser = build_parser(False)
@@ -369,16 +248,8 @@ class CLITestCase(unittest.TestCase):
         argv = ["--verbose", "--verbose", "--verbose"]
         args = parser.parse_args(argv)
         args = vars(args)
-        self.assertDictEqual(
-            args,
-            {
-                "headless": False,
-                "manifest_input": None,
-                "manifest_output": Path("bookmark-manifest.json"),
-                "media_output": Path("media"),
-                "no_clobber": False,
-                "quiet": False,
-                "num_download_threads": 8,
-                "verbose": 3,
-            },
-        )
+
+        expected = _default_expected_args()
+        expected["verbose"] = 3
+
+        self.assertDictEqual(args, expected)
